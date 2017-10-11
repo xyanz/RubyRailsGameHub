6e9d2744ce378797ae64e07c7aa629e5fb03a252
Yan Zhuk <yan.zhuk45@gmail.com>
2017-10-11 18:35:01 -0400

    add user validation for form params

--- gamehub/app/controllers/users_controller.rb
+++ gamehub/app/controllers/users_controller.rb
@@ -1,4 +1,25 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
+  def show
+    @user = User.find(params[:id])
+  end
+  # user_params from required private method below in place of params[:id]
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      puts "USER SAVED"
+      redirect_to @user
+      #do stuff
+    else
+      puts "USER DIDNT SAVE"
+      render 'new'
+    end
+  end
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
