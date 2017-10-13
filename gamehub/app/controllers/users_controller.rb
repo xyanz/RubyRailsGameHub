fa76db8f3d56da79374043c95e5fc3fa259092fd
Yan Zhuk <yan.zhuk45@gmail.com>
2017-10-13 10:15:28 -0400

    add delete user and admin priviliges

--- gamehub/app/controllers/users_controller.rb
+++ gamehub/app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update]
-  before_action :correct_user, only: [:edit, :update]
+  before_action :logged_in_user,  only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,    only: [:edit, :update]
+  before_action :admin_user,      only: :destroy
   def index
     @users = User.all
   end
@@ -9,6 +10,11 @@ class UsersController < ApplicationController
   end
   def show
     @user = User.find(params[:id])
+  end
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
   end
   # user_params from required private method below in place of params[:id]
   def create
@@ -50,5 +56,8 @@ class UsersController < ApplicationController
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless @user === current_user
+    end
+    def admin_user
+      redirect_to root_url unless current_user.admin?
     end
 end
--- gamehub/app/helpers/sessions_helper.rb
+++ gamehub/app/helpers/sessions_helper.rb
@@ -5,7 +5,7 @@ module SessionsHelper
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
   end
-  def current_user?
+  def current_user?(user)
     user == current_user
   end
   def logged_in?
